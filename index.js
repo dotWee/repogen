a0fc83eb1acd84329aa7d8e0e0308e2b9647cd87
Lukas Wolfsteiner <lukas@wolfsteiner.de>
2018-08-23 16:39:10 +0200

    Minor package refactor and cleanup

--- index.js
+++ index.js
@@ -6,44 +6,44 @@ const figlet = require('figlet');
 const xml = require('xml');
 const program = require('commander');
 
-const manifest = require('./lib/manifest');
+const manifest = require('./lib/provider/manifest');
 const inquirer = require('./lib/inquirer');
 const helper = require('./lib/helper');
 
 clear();
 console.log(
-  chalk.white(
-    figlet.textSync('repogen', {
-      horizontalLayout: 'full'
-    })
-  )
+    chalk.white(
+        figlet.textSync('repogen', {
+            horizontalLayout: 'full'
+        })
+    )
 );
 
 const run = async () => {
-  try {
-    const manif = await manifest.buildManifest();
-    console.log('manifest:', JSON.stringify(manif));
+    try {
+        const manif = await manifest.buildManifest();
+        console.log('manifest:', JSON.stringify(manif));
 
-    await manifest.writeToFile(manif);
-  } catch (err) {
-    if (err) {
-      switch (err.code) {
-        case 404:
-          console.log('This GitHub profile doesnt exist!');
-          break;
-        default:
-          console.log(err);
-      }
+        await manifest.writeToFile(manif);
+    } catch (err) {
+        if (err) {
+            switch (err.code) {
+                case 404:
+                    console.log('This GitHub profile doesnt exist!');
+                    break;
+                default:
+                    console.log(err);
+            }
+        }
     }
-  }
 }
 
 
 program
-  .version('1.0.0')
-  .option('-p, --profile-name [value]', 'Profile name')
-  .option('-r, --remote-name [value]', 'Remote name')
-  .option('-o, --output-file [value]', 'File name')
-  .parse(process.argv);
+    .version('1.0.0')
+    .option('-p, --profile-name [value]', 'Profile name')
+    .option('-r, --remote-name [value]', 'Remote name')
+    .option('-o, --output-file [value]', 'File name')
+    .parse(process.argv);
 
 run();
